552619499a32e49ec046770a30f0b54aca7eed20
3point14thon <chris.alan.bowers@gmail.com>
2018-01-05 15:08:32 -0800

    mk_csv file that removes headers from the data set, clean_data file removes some unecissary features, inclueds fantasy league player score key

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-
+#Note that this csv file does NOT contain statistics pertaining to goalies
 df = pd.read_csv('hky_stats.csv')
-print(df.head())
+df = df.drop(['gameId','playerBirthCity','playerBirthCountry','playerBirthStateProvince','playerFirstName','playerLastName','playerNationality'],axis=1)
+df['flPoints'] = df.goals*3.0 + df.shots*0.1 + df.assists*2 + df.ppPoints + df.shPoints*2
+print df.flPoints
+print df.columns
