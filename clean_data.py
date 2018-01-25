51a1c6decfd2f6d9bb2c48de5baa507e33959350
3point14thon <chris.alan.bowers@gmail.com>
2018-01-25 15:16:03 -0800

    clean_data.py now has more eda features, graph different positions with different colors and graph that compares actual game metrics with average substitutes in order to determin values pre game

--- clean_data.py
+++ clean_data.py
@@ -18,30 +18,56 @@ def main():
     df[column].fillna(NanColumns[column].mean(),inplace=True)
   #BirthyearVDraftyear(BirthNDraft)
   #print nanCount(df)
-  GameDataEDA(df)
-  df = drop(df)
+  #GameDataEDA(df,'penaltyMinutes')
+  #df = drop(df)
+  df = df.sort_values('gameDate')
+  graphTOI(df,'penaltyMinutes')
+  #for player in df.playerName.drop_duplicates():
+   # PlayerGames = df[df.playerName==player]
+    #df.loc[df.playerName==player, 'timeOnIcePerGame'] = PlayerGames.timeOnIcePerGame.rolling(5).mean()
   df.to_csv('clean_hky_stats.csv',encoding='utf-8')
+  
+def graphTOI(df,feature):
+  i = 0
+  c = ['r','g','b','y']
+  positions = df.playerPositionCode.drop_duplicates()
+  plt.figure()
+  for position in positions:
+    PositionDf = df[df.playerPositionCode==position]
+    plt.scatter(PositionDf[feature],PositionDf.flPoints,color=c[i])
+    i += 1
+  plt.legend(positions)
+  plt.show()
 
-def GameDataEDA(X):
+def GameDataEDA(X, feature):
   names = X.playerName.drop_duplicates().iloc[:3]
   for name in names:
     PlayerStat = X[X.playerName==name].sort_values(by='gameDate')
     PlayerStat.index = range(len(PlayerStat))
     for i in PlayerStat.index:
-      PlayerStat.at[i,'averageTOI'] = PlayerStat.timeOnIcePerGame.iloc[:i].mean()
-    print PlayerStat
+      PlayerStat.at[i,'average'] = PlayerStat[feature].iloc[:i].mean()
+    PlayerStat['rollingAvg'] = PlayerStat[feature].rolling(5).mean()
+    PlayerStat['rollingAvg3'] = PlayerStat[feature].rolling(3).mean()
     plt.figure()
-    plt.scatter(PlayerStat.gameDate,PlayerStat.averageTOI,color='r')
-    plt.scatter(PlayerStat.gameDate,PlayerStat.timeOnIcePerGame)
+    plt.scatter(PlayerStat.gameDate,PlayerStat.rollingAvg3,color='y')
+    plt.scatter(PlayerStat.gameDate,PlayerStat.rollingAvg,color='g')
+    plt.scatter(PlayerStat.gameDate,PlayerStat.average,color='r')
+    plt.scatter(PlayerStat.gameDate,PlayerStat[feature])
+    plt.title(name)
+    plt.figure()
+    plt.scatter(PlayerStat.flPoints,PlayerStat.rollingAvg3,color='y')
+    plt.scatter(PlayerStat.flPoints,PlayerStat.rollingAvg,color='g')
+    plt.scatter(PlayerStat.flPoints,PlayerStat.average,color='r')
+    plt.scatter(PlayerStat.flPoints,PlayerStat[feature])
     plt.title(name)
   plt.show()
 
 # Takes in the main data frame and returns a streamlined dataframe
 def drop(X):
-  #droping unnecisary or redundent features
-  X = X.drop(['Unnamed: 0','gamesPlayed','gameId','playerBirthCity','playerBirthCountry','plusMinus','playerId','playerBirthStateProvince','shootingPctg','playerFirstName','playerLastName','playerNationality','playerDraftYear'],axis=1) 
+  #droping unnecisary features, redundent features, or features directly related   to the target variable
+  X = X.drop(['Unnamed: 0','gamesPlayed','gameId','playerBirthCity','playerBirthCountry','plusMinus','playerId','playerBirthStateProvince','shootingPctg','playerFirstName','playerLastName','playerNationality','playerDraftYear''assists','gameWinningGoals','goals','otGoals','points','ppGoals','ppPoints','shGoals','shPoints'],axis=1) 
   #sepparating out features I want to deal with later, figure out if timeOnIcePerGame is an average of all games or per the game in question
-#  X = X.drop(['assists','gameWinningGoals','goals','otGoals','penaltyMinutes','points','ppGoals','ppPoints','shGoals','shPoints','shiftsPerGame','shots','timeOnIcePerGame'], axis=1)
+#  X = X.drop(['penaltyMinutes','shiftsPerGame','shots','timeOnIcePerGame'], axis=1)
   return X
 
 #gives the number of NaNs in every column in a Data frame
