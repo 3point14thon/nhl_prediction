a6f2cc27340c1066ea850bb492a1d0c2d686b255
3point14thon <chris.alan.bowers@gmail.com>
2018-02-21 14:25:40 -0800

    Got rid of unnecissary files and added a much needed readme

--- hky_EDA.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def main():
-  df = pd.read_csv('clean_hky_stats.csv')
-
-
-def 
--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-rollingmean = pd.DataFrame([2,4,2,6,8,645,6])
-print rollingmean
-rollingmean = rollingmean.drop(len(rollingmean)-1,axis=0)
-rollingmean.loc[-1] = np.nan
-rollingmean = rollingmean.sort_index()
-print rollingmean
