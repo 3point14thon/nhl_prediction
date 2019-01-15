55276d0ccb5a0d693b68ab13445cbba8188230c6
3point14thon <chris.alan.bowers@gmail.com>
2019-01-14 18:25:47 -0800

    added clear doc strings and put code in a function

--- mk_csv.py
+++ mk_csv.py
@@ -1,20 +1,22 @@
-#Intended to write a CSV file containing the statistics of current NHL players
-#the URL might have to be renewed I'm unsure if data is added to it or a new
-#one is made when new data becomes available
 import pandas as pd
 import requests
 import json
 
-#visit this site to get a new URL if needed, http://www.nhl.com/stats/player?reportType=game&dateFrom=2017-10-04&dateTo=2018-01-02&gameType=2&filter=gamesPlayed,gte,1&sort=points,goals,assists
 
-#Imports data from website then reduces it to a dictionary of dictionaries containing the data
-HkDic = requests.get('http://www.nhl.com/stats/rest/skaters?isAggregate=false&reportType=basic&isGame=true&reportName=skatersummary&sort=[{%22property%22:%22points%22,%22direction%22:%22DESC%22},{%22property%22:%22goals%22,%22direction%22:%22DESC%22},{%22property%22:%22assists%22,%22direction%22:%22DESC%22}]&cayenneExp=gameDate%3E=%222017-10-04%22%20and%20gameDate%3C=%222018-01-02%22%20and%20gameTypeId=2')
-HkDic = json.loads(HkDic.text)
-HkDic = HkDic['data']
+def main(url):
+  '''
+  Intended to write a CSV file containing the statistics of current NHL players
 
-#Converts one of the dictionaries to a data frame then does the same for the rest and appends them
-#I tried in place methodes for appending the data like .loc but this seems to be fastest
-df = pd.DataFrame(HkDic[0],index=[0])
-for i in range(1,len(HkDic)):
-  df = df.append(pd.DataFrame(HkDic[i],index=[i]))
-df.to_csv('hky_stats.csv',encoding='utf-8')
+  Args:
+    url: the url to request from
+
+  Returns: None
+  '''
+  hkdic = requests.get(url)
+  df = pd.DataFrame.from_dict(hkdic.json()['data'])
+  df.to_csv('hky_stats.csv',encoding='utf-8')
+
+
+if __name__ == '__main__':
+  #url may need to be updated for most recent statistics
+  main('http://www.nhl.com/stats/rest/skaters?isAggregate=false&reportType=basic&isGame=true&reportName=skatersummary&sort=[{%22property%22:%22points%22,%22direction%22:%22DESC%22},{%22property%22:%22goals%22,%22direction%22:%22DESC%22},{%22property%22:%22assists%22,%22direction%22:%22DESC%22}]&cayenneExp=gameDate%3E=%222017-10-04%22%20and%20gameDate%3C=%222018-01-02%22%20and%20gameTypeId=2')
